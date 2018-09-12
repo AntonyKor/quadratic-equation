205a9d0ada599cc1f3d5891cdbc71c1d28b4cc3c
AntonyKor <antonykor@gmail.com>
2018-09-12 18:30:46 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let clearEquation = equation.split('').filter((item) => item!==" ").join(''),
+    a = +clearEquation.slice(0,clearEquation.indexOf("*"));
+    clearEquation = clearEquation.slice(clearEquation.indexOf("x")+3);
+    b = +clearEquation.slice(0,clearEquation.indexOf("*"));
+    clearEquation = clearEquation.slice(clearEquation.indexOf("x")+1);
+    c = +clearEquation;
+    let d = b*b - 4*a*c;
+    console.log(a+' '+b+' '+c+'   '+d);
+    return [((-b+Math.sqrt(d))/(2*a)).toFixed(0),((-b-Math.sqrt(d))/(2*a)).toFixed(0)].sort((left, right) => left - right);
 }
